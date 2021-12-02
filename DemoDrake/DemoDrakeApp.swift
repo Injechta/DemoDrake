407526758840ca7640018ae72b98ec8e4cf2e057
Injechta <injechta@gmail.com>
2021-12-02 14:36:58 +0100

    Initial Commit

---
+++ DemoDrake/DemoDrakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoDrakeApp.swift
+//  DemoDrake
+//
+//  Created by gregoryelbajoury on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoDrakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
