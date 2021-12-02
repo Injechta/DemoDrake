2aeff0113d9adf419b19bee4c262d0f7af143628
Injechta <injechta@gmail.com>
2021-12-02 15:12:57 +0100

    ajout écran albumview

---
+++ DemoDrake/AlbumViewDetail.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumViewDetail.swift
+//  DemoDrake
+//
+//  Created by gregoryelbajoury on 02/12/2021.
+//
+
+import SwiftUI
+
+struct AlbumViewDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumViewDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumViewDetail()
+    }
+}
